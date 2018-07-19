21fdcb9cd0f49940d9bf2e99670acfa25b96ea49
Kate1234567891 <katehalle617@icloud.com>
2018-07-19 19:36:07 +0000

    automatically backed up by learn

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,3 @@
 class Question
-question_1 = Question.new
-question_1.id = "Q1"
-question_1.text = "Hi, welcome to my website! I created this website because I want to allow people to be able to go to my website and find an issue that you can help! The first thing I want to ask you is would you like to help people or the planet?"
-question_1.answers = {
-  "Planets" => "Q2",
-  "People" => "Q3" 
-}
+  attr_accessor
 end
