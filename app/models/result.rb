c9a7fda123a7e9beed66407a8ee61bb2087d3389
Kate1234567891 <katehalle617@icloud.com>
2018-07-19 19:40:07 +0000

    automatically backed up by learn

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,4 +1,9 @@
 class Result
-yes_result = Result.new(:id => "R1", :text => "Yes, go out.")
-no_result = Result.new(:id => "R2", :text => "No, don't go out.")
+  attr_accessor :id, :text
+
+  def initialize(data = {})
+    @id = data[:id]
+    @text = data[:text]
+  end
+
 end
